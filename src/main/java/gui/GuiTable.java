91fa8315554f9ecf9d868b3519336c7a5368ab32
Rec Ferenc <rex@slider.ki.iif.hu>
2018-04-03 19:30:42 +0200

    Fix tictactoe array bug

--- src/main/java/gui/GuiTable.java
+++ src/main/java/gui/GuiTable.java
@@ -11,13 +11,25 @@ import java.awt.event.MouseListener;
 public class GuiTable extends JFrame implements MouseListener{
 
     Container cp = getContentPane();
-    JLabel [][] jLabels = new JLabel[3][3];
+    JLabel [][] jLabels = new JLabel[20][20];
     final Color emptyColor = Color.WHITE;
     final Color testColor = Color.RED;
     GameController gameController;
+    int ticTacToeTableSize;
 
-    public GuiTable(GameController gameController){
+    public void setTicTacToeTableSize(int ticTacToeTableSize) {
+        if(ticTacToeTableSize > 4 && ticTacToeTableSize < 1001){
+            this.ticTacToeTableSize = ticTacToeTableSize;
+        }else{
+            System.out.println("Please set between 3 and 1000");
+        }
+
+    }
+
+
+    public GuiTable(GameController gameController, int ticTacToeTableSize){
         this.gameController = gameController;
+        setTicTacToeTableSize(ticTacToeTableSize);
         initGui();
 
     }
@@ -25,8 +37,8 @@ public class GuiTable extends JFrame implements MouseListener{
     public void initGui(){
 
         setTitle("TicTacToeGame");
-        setPreferredSize(new Dimension(300,200));
-        JPanel tablePanel = new JPanel(new GridLayout(3,3));
+        setPreferredSize(new Dimension(1024,1024));
+        JPanel tablePanel = new JPanel(new GridLayout(20,20));
 
         Border paddingBorder = BorderFactory.createEmptyBorder(0,0,0,0);
         Border border = BorderFactory.createLineBorder(Color.BLACK);
@@ -35,7 +47,7 @@ public class GuiTable extends JFrame implements MouseListener{
             for(int j = 0; j<jLabels[0].length;j++){
                 jLabels[i][j] = new JLabel();
                 jLabels[i][j].setOpaque(true);
-                jLabels[i][j].setName(i+""+j);
+                jLabels[i][j].setName(i+"-"+j); //we have to split it in the controller class
                 jLabels[i][j].addMouseListener(this);
                 jLabels[i][j].setBackground(emptyColor);
                 jLabels[i][j].setBorder(BorderFactory.createCompoundBorder(border,paddingBorder));
--- src/main/java/trial/GameController.java
+++ src/main/java/trial/GameController.java
@@ -10,23 +10,18 @@ public class GameController {
     Counter counterPlayer1,counterPlayer2 = null;
     int matrix1=1, matrix2=1;
     int flag = 0;
-    final int NUMBEROFDOTCOUNT = 3;
-    int[][] ticTacToeMatrix = {
-            {0, 0, 0},
-            {0, 0, 0},
-            {0, 0, 0},
+    final int NUMBEROFDOTCOUNT = 5;
+    final int TICTACTOEGAMETABLESIZE = 20;
 
-    };
+    int[][] ticTacToeMatrix = new int[TICTACTOEGAMETABLESIZE][TICTACTOEGAMETABLESIZE];
     boolean win = false;
     String winnerName = "";
     MatrixWorker matrixWorker = new MatrixWorker(ticTacToeMatrix);
     Player player1 = new Player("Rex", "X");
     Player player2 = new Player("Moni", "O");
     Table table = new Table(matrixWorker);
-    int player1NumberInMatrix = player1.getPiece().equals("X")?1:2;
-    int player2NumberInMatrix = player2.getPiece().equals("X")?1:2;
 
-    GuiTable guiTable = new GuiTable(this);
+    GuiTable guiTable = new GuiTable(this,TICTACTOEGAMETABLESIZE);
 
     public GameController() {
 
@@ -39,21 +34,22 @@ public class GameController {
         switch (command){
             case "click":
                 doCheckAndSet(panel);
-
                 break;
         }
     }
 
     private void doCheckAndSet(JLabel panel) {
         String row,column;
+        String [] stringArrayFromSplit;
         counterPlayer1 = new Counter(matrixWorker, player1);
         counterPlayer2 = new Counter(matrixWorker, player2);
 
         if(panel.getBackground() != Color.BLUE && panel.getBackground() != Color.RED) {
 
-            row = panel.getName().substring(0, 1);
-            column = panel.getName().substring(1, 2);
-
+            //because of xx,yyy,zzzz digits, for example: col 11, row 11...
+            stringArrayFromSplit = panel.getName().split("-");
+            row = stringArrayFromSplit[0];
+            column = stringArrayFromSplit[1];
             matrixWorker.setPosition(Integer.parseInt(row) + 1,Integer.parseInt(column) + 1);
 
             if (flag == 0) {
@@ -96,90 +92,6 @@ public class GameController {
         new GameController();
     }
 
-    public void runGame() {
-        int[][] ticTacToeMatrixTest = {
-                {1, 1, 2},
-                {0, 1, 1},
-                {1, 1, 1},
-
-        };
-        int[][] ticTacToeMatrixTest2 = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9},
-
-        };
-
-
-
-
-
-
-
-
-  /*
-        boolean win = false;
-        String winnerName = "";
-        //System.out.println("MATRIX1: " + matrix1);
-        //System.out.println("MATRIX2: " + matrix2);
-
-        if (flag == 0) {
-            System.out.print(player1.getName() + " row: column: ");
-
-            matrixWorker.setPosition(matrix1, matrix2);
-            matrixWorker.setCellValue(player1NumberInMatrix); // XorO, X=1,O=2
-            flag = 1;
-        } else {
-            System.out.print(player2.getName() + " row: column: ");
-
-            matrixWorker.setPosition(matrix1, matrix2);
-            matrixWorker.setCellValue(player2NumberInMatrix); // XorO, X=1,O=2
-
-            flag = 0;
-        }
-        */
-        //#### CONSOLE GAME
-        /*
-        while (matrixWorker.freeCellNumber > 0 && !win) {
-
-            table.drawTable();
-
-            if (flag == 0) {
-                System.out.print(player1.getName() + " row: column: ");
-                matrix1 = sc.nextInt();
-                matrix2 = sc.nextInt();
-                matrixWorker.setPosition(matrix1, matrix2);
-                matrixWorker.setCellValue(player1NumberInMatrix); // XorO, X=1,O=2
-                flag = 1;
-            } else {
-                System.out.print(player2.getName() + " row: column: ");
-                matrix1 = sc.nextInt();
-                matrix2 = sc.nextInt();
-                matrixWorker.setPosition(matrix1, matrix2);
-                matrixWorker.setCellValue(player2NumberInMatrix); // XorO, X=1,O=2
-
-                flag = 0;
-            }
-            win = isWin(counterPlayer1) || isWin(counterPlayer2);
-            if(win && flag == 0){
-                winnerName = player2.getName();
-            }else if(win && flag == 1){
-                winnerName = player1.getName();
-            }
-            System.out.println();
-        }
-
-        table.drawTable();
-
-        if(winnerName.isEmpty()){
-            System.out.println("Nobody won because there is no empty cell");
-            System.out.println("End of Game");
-        }else {
-            System.out.println(winnerName + " is won");
-            System.out.println("End of Game");
-        }
-*/
-    }
 
         public void printArray(int[][] arrayName){
         int i,j;
